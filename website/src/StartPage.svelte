e6d77c9643d82e4e5e04289e93bef75e85dc7d46
anish-lkakapragada <anish.lakkapragada@gmail.com>
2022-01-30 23:28:15 -0800

    reactive changes

--- website/src/StartPage.svelte
+++ website/src/StartPage.svelte
@@ -1,39 +1,79 @@
 <script lang="ts"> 
     import "smelte/src/tailwind.css" ;
-    //import {TextField, Slider} from "smelte";
-    import {TextField} from "sozai";
+    import {TextField, Slider} from "smelte";
     import {isWikipediaTopic} from "./utils.js";
-    import {createEventDispatcher} from "svelte";
-    const dispatcher = createEventDispatcher(); 
+    import {createEventDispatcher, tick} from "svelte";
     let success = false; 
     let numTries = 10; 
 
-    let startColor; 
-
-    let tf = null; 
-
+    // initialize the janky stuff 
+    let startColor, endColor; 
     let topicStart = null;
     let topicEnd = null;
-    const isValid = async (topic, isStart, garb) => { 
-        if (isStart) {
-            startColor = "error"; 
-        }
-    }
+    let startRender = true; 
+    let endRender = true; 
 
-    $: if (topicStart != null) {isValid(topicStart, true, startColor)}; 
+    const validTopic = async (topic) => {
+        if (topic == null) {
+            return true; 
+        }
+
+        const isTopic = await isWikipediaTopic(topic); 
+        if (!isTopic) {
+            return false; 
+        }
+
+        return true; 
+    }
+    
+    // stuff do be janky 
+    const changeColor = async (topic, isStart) => {
+        const isValid = await validTopic(topic);
+
+        if (isValid) {
+            if (isStart) {startColor="success";}
+            else {endColor="success";}
+        }
+
+        
+        else {
+            if (isStart) {startColor="error";}
+            else {endColor="error";}
+        }
+
+
+        if (isStart){
+            startRender = false; 
+        }
+
+        else {
+            endRender = false; 
+        }
+
+
+
+        await tick(); 
+
+        startRender =true; 
+        endRender = true;
+    }
 
 
 </script> 
 
-<html lang="en"> 
-    <div class="text-center font-sans mx-20"> 
-        <h1 class="text-5xl"> Wikipedia Game (By yourself!) </h1>
-        <p class="text-2xl "> In the wikipedia game, you try to go from one article to another only using the hyperlinks on the page, as fast as you can. </p>
 
-        <h2 class="text-xl"> Enter First Topic </h2>
-        <TextField class=" " filled on:blur={() => {isValid(topicStart, true)}} bind:color={startColor} bind:value={topicStart} label="Starting Topic"  />  
-        <TextField class="w-2" bind:value={topicEnd} label="Ending Topic" color="primary" />
-    </div>
-</html>
+<div class="text-center font-sans mx-20"> 
+    <h1 class="text-5xl"> Wikipedia Game (By yourself!) </h1>
+    <p class="text-2xl "> In the wikipedia game, you try to go from one article to another only using the hyperlinks on the page, as fast as you can. </p>
+
+    {#if startRender}
+        <TextField color={startColor} on:blur={() => {changeColor(topicStart, true)}} label="Starting Topic" bind:value={topicStart}/>  
+    {/if}
+
+    {#if endRender}
+        <TextField color={endColor} on:blur={() => {changeColor(topicEnd, false)}} label="Ending Topic" bind:value={topicEnd}/>  
+    {/if}
+    
+</div>
 
 
